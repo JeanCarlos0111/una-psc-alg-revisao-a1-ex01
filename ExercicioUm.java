7d7310908a20267875ac3a7927fbf2044019b224
JeanCarlos0111 <130711959+JeanCarlos0111@users.noreply.github.com>
2023-04-27 23:57:39 -0300

    Add files via upload

--- ExercicioUm.java
+++ ExercicioUm.java
@@ -5,7 +5,7 @@ public class ExercicioUm {
         Scanner sc = new Scanner(System.in);
         String nome, cartaoVacina, sintomasRecentes, contatoSintomaticos, viagemExterior;
         int idade;
-        boolean respostaValida;
+        int contador = 0;
         int porcentagemRisco = 0;
         
         System.out.println("**** CHECKUP INFLUENZA****\n");
@@ -17,55 +17,69 @@ public class ExercicioUm {
         idade = sc.nextInt();
         sc.nextLine(); 
         
-        do {
             System.out.print("Seu cartão de vacina está em dia? (SIM / NAO) ");
             cartaoVacina = sc.nextLine().toUpperCase();
-            respostaValida = cartaoVacina.equals("SIM") || cartaoVacina.equals("NAO");
-            if (!respostaValida) {
+            while (!(cartaoVacina.equals("SIM")) && !(cartaoVacina.equals("NAO"))){
                 System.out.println("Resposta inválida. Por favor, insira SIM ou NAO.");
+                cartaoVacina = sc.nextLine().toUpperCase();
+                contador++;
+                if (contador >= 3){
+                    System.out.println("Não foi possível realizar o diagnostico.\nGentileza procurar ajuda médica caso apareça algum sintoma.\n");
+                    System.exit(0);
+                }
             }
-        } while (!respostaValida);
-
-        
-        do {
+            if(cartaoVacina.equals("NAO")){
+                porcentagemRisco += 10;
+            }
+            System.out.println(porcentagemRisco);
+            contador = 0;
             System.out.print("Teve algum dos sintomas recentemente? (dor de cabeça, febre, náusea, dor articular, gripe) (SIM / NAO) ");
             sintomasRecentes = sc.nextLine().toUpperCase();
-            respostaValida = sintomasRecentes.equals("SIM") || sintomasRecentes.equals("NAO");
-            if (!respostaValida) {
+            while (!(sintomasRecentes.equals("SIM")) && !(sintomasRecentes.equals("NAO"))){
                 System.out.println("Resposta inválida. Por favor, insira SIM ou NAO.");
+                sintomasRecentes = sc.nextLine().toUpperCase();
                 contador++;
-            } else if (sintomasRecentes.equals("SIM")) {
+                if (contador >= 3){
+                    System.out.println("Não foi possível realizar o diagnostico.\nGentileza procurar ajuda médica caso apareça algum sintoma.\n");
+                    System.exit(0);
+                }
+            }
+            if(sintomasRecentes.equals("SIM")) {
                 porcentagemRisco += 30;
             }
-        } while (!respostaValida);
-
-        do {
+            System.out.println(porcentagemRisco);
+            contador = 0;
             System.out.print("Teve contato com pessoas com sintomas gripais nos últimos dias? (SIM / NAO) ");
             contatoSintomaticos = sc.nextLine().toUpperCase();
-            respostaValida = contatoSintomaticos.equals("SIM") || contatoSintomaticos.equals("NAO");
-            if (!respostaValida) {
+            while (!(contatoSintomaticos.equals("SIM")) && !(contatoSintomaticos.equals("NAO"))){
                 System.out.println("Resposta inválida. Por favor, insira SIM ou NAO.");
-            } else if (contatoSintomaticos.equals("SIM")) {
+                contatoSintomaticos = sc.nextLine().toUpperCase();
+                contador++;
+                if (contador >= 3){
+                    System.out.println("Não foi possível realizar o diagnostico.\nGentileza procurar ajuda médica caso apareça algum sintoma.\n");
+                    System.exit(0);
+                }
+            }
+            if (contatoSintomaticos.equals("SIM")){
                 porcentagemRisco += 30;
             }
-        } while (!respostaValida);
-        
-        do {
+            System.out.println(porcentagemRisco);    
+            contador = 0;
             System.out.print("Está retornando de viagem realizada no exterior? (SIM / NAO) ");
             viagemExterior = sc.nextLine().toUpperCase();
-            respostaValida = viagemExterior.equals("SIM") || viagemExterior.equals("NAO");
-            if (!respostaValida) {
+            while (!(viagemExterior.equals("SIM")) && !(viagemExterior.equals("NAO"))){
                 System.out.println("Resposta inválida. Por favor, insira SIM ou NAO.");
-            } else if (viagemExterior.equals("SIM")) {
-                porcentagemRisco += 30;
-                System.out.println("Você ficará sob observação por 05 dias.");
+                viagemExterior = sc.nextLine().toUpperCase();
+                contador++;
+                if (contador >= 3){
+                    System.out.println("Não foi possível realizar o diagnostico.\nGentileza procurar ajuda médica caso apareça algum sintoma.\n");
+                    System.exit(0);
+                }
             }
-        } while (!respostaValida);
-        
-        if (cartaoVacina.equals("NAO")) {
-            porcentagemRisco += 10;
-        }
-        
+            if(viagemExterior.equals("SIM")){
+                porcentagemRisco += 30;
+            }
+            System.out.println(porcentagemRisco);
         System.out.println("\n**** RESULTADO DO CHECKUP INFLUENZA ****\n");
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
@@ -74,13 +88,15 @@ public class ExercicioUm {
         System.out.println("Teve contato com pessoas infectadas? " + contatoSintomaticos);
         System.out.println("Retornando de viagem realizada no exterior? " + viagemExterior);
 
-        System.out.println("\nProbabilidade dessa pessoa estar infectada: " + porcentagemRisco + "%");
+        System.out.println("\nA probabilidade de você estar infectado(a) é: " + porcentagemRisco + "%");
 
         if (porcentagemRisco <= 30) {
             System.out.println("Paciente sob observação. Caso apareça algum sintoma, gentileza buscar assistência médica.");
         } else if (porcentagemRisco <= 60) {
-            System.out.println("Paciente com risco de estar infectado. Gentileza aguardar em lockdown por 02 dias para ser acompanhado.");
-        } else if (porcentagemRisco >= 90) {
+            System.out.println("Paciente com risco de estar infectado. Gentileza aguardar em lockdown por 2 dias para ser acompanhado.");
+        } else if (porcentagemRisco >= 60){
+            System.out.println("Paciente com risco de estar infectado. Gentileza aguardar em lockdown por 2 dias para ser acompanhado.");
+        }else if (porcentagemRisco >= 90) {
             System.out.println("Paciente crítico! Gentileza aguardar em lockdown por 10 dias para ser acompanhado.");
         }
         
